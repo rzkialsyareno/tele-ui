9e7aa5ac8db686d64b62f650cf7b0bd1a8713c8d
rzkialsyareno <rzkialsyareno@gmail.com>
2026-03-04 21:48:29 +0700

    Update PORT to use process.env

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const fs = require("fs");
 const path = require("path");
 const zlib = require("zlib");
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 const CARDS_DIR = path.join(__dirname, "cards");
 
 // Ensure cards directory exists
